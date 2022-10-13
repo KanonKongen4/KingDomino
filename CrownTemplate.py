4b5f312d1933ca7ff5fb6a368e511b3c379ca3b3
marchusl <marchusl@gmail.com>
2022-10-13 14:06:57 +0200

    we can find crowns

--- CrownTemplate.py
+++ CrownTemplate.py
@@ -1,6 +1,18 @@
 import cv2 as cv
 import numpy
 
-imgtemp = cv.imread("crown.jpg")
-imgtemp2 = imgtemp.copy()
-template = cv.imread("20.jpg")
+template = cv.imread('crownblack.jpg', 0)
+imgsearch = cv.imread('20.jpg')
+img_gray = cv.cvtColor(imgsearch, cv.COLOR_BGRA2GRAY)
+w, h = template.shape[::-1]
+
+res = cv.matchTemplate(img_gray, template, cv.TM_CCOEFF_NORMED)
+threshold = 0.90
+loc = numpy.where(res >= threshold)
+for pt in zip(*loc[::-1]):
+    cv.rectangle(imgsearch, pt, (pt[0] + w, pt[1] + h), (0, 0, 225), 2)
+
+cv.imwrite('res.png', imgsearch)
+cv.imshow('plsvirk', imgsearch)
+
+cv.waitKey(0)
