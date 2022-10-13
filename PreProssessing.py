2c4b6ec06a6bde7befeae35b1401660aa40c67ce
KanonKongen4 <91434226+KanonKongen4@users.noreply.github.com>
2022-10-13 12:42:17 +0200

    Restructuring of Code!
    Now the division class is a seperate file and the preproccessing steps are combined in the "preProssessing" class

--- PreProssessing.py
+++ PreProssessing.py
@@ -1,26 +1,11 @@
 import cv2 as cv
 import numpy
+import HelperFunctions
+import DivideimageIntoParts
 
 img = cv.imread("20.jpg")
 cv.imshow("Start", img)
 
-
-def get_tile_size_of_img(input_image):
-    height = input_image.shape[0]
-    # length = input_image.shape[1]
-    size_of_tile = height / 5
-    return size_of_tile
-
-
-def divide_image_into_equal_parts(input_image):
-    h, w, channels = input_image.shape
-    fifth = w // 5
-    firstPart = input_image[0:100, 0:100 ]
-    cv.imshow(f"Left part{0}", firstPart)
-    firstPart = input_image[100:200, 0:100]
-    cv.imshow(f"Left part{2}", firstPart)
-
-
-divide_image_into_equal_parts(img)
+DivideimageIntoParts.divide_image(img)
 
 key = cv.waitKey(0)
