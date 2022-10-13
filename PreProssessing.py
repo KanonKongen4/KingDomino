726ed4ec0eb75aa740fbcffe2f8c96e4939770de
KanonKongen4 <91434226+KanonKongen4@users.noreply.github.com>
2022-10-13 12:15:53 +0200

    Dividing Image into 25 equal parts now works!

--- PreProssessing.py
+++ PreProssessing.py
@@ -13,12 +13,21 @@ def get_tile_size_of_img(input_image):
 
 
 def divide_image_into_equal_parts(input_image):
+    for row in range(1, 6):
+        divide_row(input_image, row)
+
+def divide_row(input_image, rowNum):
     h, w, channels = input_image.shape
     fifth = w // 5
-    firstPart = input_image[0:100, 0:100 ]
-    cv.imshow(f"Left part{0}", firstPart)
-    firstPart = input_image[100:200, 0:100]
-    cv.imshow(f"Left part{2}", firstPart)
+    multiplierStart = 0
+    multiplier = 1
+
+    for x in range(5):
+        print(f"x is{x}")
+        firstPart = input_image[100 * (rowNum-1) :100 * rowNum, fifth * multiplierStart:fifth * multiplier]
+        cv.imshow(f"rowNum: {rowNum}, part{multiplier}", firstPart)
+        multiplier += 1
+        multiplierStart += 1
 
 
 divide_image_into_equal_parts(img)
