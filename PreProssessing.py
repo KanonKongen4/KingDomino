c0d0e919b335e9f38ea4d03e7c0a87c56041601e
KanonKongen4 <91434226+KanonKongen4@users.noreply.github.com>
2022-10-13 11:35:01 +0200

    Base code that works"

--- PreProssessing.py
+++ PreProssessing.py
@@ -4,28 +4,22 @@ import numpy
 img = cv.imread("20.jpg")
 cv.imshow("Start", img)
 
+
 def get_tile_size_of_img(input_image):
     height = input_image.shape[0]
-    length = input_image.shape[1]
-    print(f"height is {height}, length is {length}")
-
-    size_of_tile = height/5
-    print(f"tileSize is {size_of_tile}")
+    # length = input_image.shape[1]
+    size_of_tile = height / 5
     return size_of_tile
+
 
 def divide_image_into_equal_parts(input_image):
     h, w, channels = input_image.shape
-    fifth = w//5
+    fifth = w // 5
+    firstPart = input_image[0:100, 0:100 ]
+    cv.imshow(f"Left part{0}", firstPart)
+    firstPart = input_image[100:200, 0:100]
+    cv.imshow(f"Left part{2}", firstPart)
 
-    firstPart = input_image[:fifth, :fifth]
-    cv.imshow('Left part', firstPart)
-    #print(f"input_image[pointx,pointy is {input_image[pointx,pointy]}")
-
-
-#def GetSmallerArrayFromArray(tileSize, Startingpoint)2
-    #arrayOf2DArrays = numpy.array()
-
-get_tile_size_of_img(img)
 
 divide_image_into_equal_parts(img)
 
