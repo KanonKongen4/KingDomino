702361c17bf04f4a095a9d0e59022ad2ad7c70b5
KanonKongen4 <91434226+KanonKongen4@users.noreply.github.com>
2022-11-18 11:47:22 +0100

    did stuff

--- PreProssessing.py
+++ PreProssessing.py
@@ -1,34 +1,35 @@
 import cv2 as cv
-
-import numpy
-import HelperFunctions
-import DivideimageIntoParts
-
+import DominantColourFinder
 import HelperFunctions
 import DivideimageIntoParts
 import PrepareTileForTest
+import TerritoryName
 
-img = cv.imread("20.jpg")
+img = cv.imread("20.jpg")#Read the test image
+blurred = PrepareTileForTest.blur_image(img) # blur the input image
+cv.imshow("blurred", blurred)# show the input image
 
-DivideimageIntoParts.divide_image(img)
+listOfTiles = DivideimageIntoParts.divide_image(blurred) #Divide the image into 25 tiles
+#HelperFunctions.show_images_in_list(listOfTiles) # Show the divided tiles!
 
-listOfTiles = DivideimageIntoParts.divide_image(img)
 
-listOfTiles = PrepareTileForTest.return_list_of_blurred_images(listOfTiles)
+colours = DominantColourFinder.get_list_of_dominant_colours(listOfTiles) # colours of the tiles
 
-HelperFunctions.show_images_in_list(listOfTiles)
-
-img2 = PrepareTileForTest.blur_image(img)
-cv.imshow("name", img2)
-
-colours = PrepareTileForTest.get_list_of_dominant_colours(listOfTiles)
-
-# TODO This is not working!!
+one_colour_images = []
 i = 0
 for colour in colours:
-    HelperFunctions.create_image_with_colour(colour, i)
+    one_colour_images.append(HelperFunctions.create_image_with_colour(colour, i))
     i += 1
 
-# list_2D_territory_codes = [[]]
+stringArray = HelperFunctions.create_empty_string_array()
+
+list_2D_territory_code_strings = [[]]
+
+i = 0
+for image in one_colour_images:
+    territoryString = TerritoryName.get_territory_name(image,i)
+    i += 1
+
+
 
 key = cv.waitKey(0)
---
+++ TerritoryName.py
@@ -0,0 +1,79 @@
+import cv2 as cv
+import colorsys
+
+def get_territory_name(img, id):
+    hsv = cv.cvtColor(img, cv.COLOR_BGR2HSV)
+    color_hsv = hsv[0][0]
+
+    range = 7
+
+    # TODO: get the average colours for each tile by getting the average of a number of differently lighted images.
+
+    col_water = (100, 0, 180)
+    col_forest = (39, 0, 66)
+    col_meadow = (39, 0, 160)
+    col_desert = (23, 0, 116)
+    col_coal = (23, 0, 70)
+    col_corn = (26, 0, 198)
+
+    string = "empty"
+
+    if (check_conditions(color_hsv, col_water, 6, 70)):
+        string = "water"
+    elif (check_conditions(color_hsv, col_forest, 6, 70)):
+        string = "forest"
+    elif (check_conditions(color_hsv, col_meadow, 6, 70)):
+        string = "meadow"
+    elif (check_conditio
+            ns(color_hsv, col_desert, 6, 25)):
+        string = "desert"
+    elif (check_conditions(color_hsv, col_coal, 6, 25)):
+        string = "coal"
+    elif (check_conditions(color_hsv, col_corn, 6, 40)):
+        string = "corn"
+
+    print(str(string) + "  " + str(id) +"  hsv: " + str(hsv[0][0])  )
+
+    return string
+
+def get_territory_name_from_colour(color, id):
+    hsv = colorsys.rgb_to_hsv(color)
+
+    range = 7
+
+    # TODO: get the average colours for each tile by getting the average of a number of differently lighted images.
+
+    col_water = (100, 0, 180)
+    col_forest = (39, 0, 66)
+    col_meadow = (39, 0, 160)
+    col_desert = (23, 0, 116)
+    col_coal = (23, 0, 70)
+    col_corn = (26, 0, 198)
+
+    string = "empty"
+
+    if (check_conditions(hsv, col_water, 6, 70)):
+        string = "water"
+    elif (check_conditions(hsv, col_forest, 6, 70)):
+        string = "forest"
+    elif (check_conditions(hsv, col_meadow, 6, 70)):
+        string = "meadow"
+    elif (check_conditions(hsv, col_desert, 6, 25)):
+        string = "desert"
+    elif (check_conditions(hsv, col_coal, 6, 25)):
+        string = "coal"
+    elif (check_conditions(hsv, col_corn, 6, 40)):
+        string = "corn"
+
+    print(str(string) + "  " + str(id) +"  hsv: " + str(hsv[0][0])  )
+
+    return string
+
+def check_conditions(input_color, color_to_find, range_hue, range_value):
+    hue = input_color[0]
+
+    value = input_color[2]
+
+    if hue > color_to_find[0] - range_hue and hue < color_to_find[0] + range_hue:
+        if value > color_to_find[2] - range_value and value < color_to_find[2] + range_value:
+            return True
