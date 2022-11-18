113d81f0298bd580b8ec4ce5b0cbb319b7c573c5
aightImmaProgram <mobn21@student.aau.dk>
2022-11-18 14:45:43 +0100

    Grassfire (Work in progress)
    Grassfire function that (hopefully) can take in a 5x5 matrix filled with strings and will assemble blobs of tiletypes by giving them the same ID

--- grassfire.py
+++ grassfire.py
@@ -13,29 +13,29 @@ img = cv.imread("shapes.png", cv.IMREAD_GRAYSCALE)
                [0, 0, 255, 255, 0, 0]], dtype=np.uint8)'''
 
 
-def ignite_pixel(image, coordinate, id):
+def ignite_pixel(stringMatrix, coordinate, id, tileTypeString):
     y, x = coordinate
     burn_queue = deque()
 
-    if image[y, x] == 255:
+    if stringMatrix[y, x] == tileTypeString:
         burn_queue.append((y, x))
 
     while len(burn_queue) > 0:
         current_coordinate = burn_queue.pop()
         y, x = current_coordinate
-        if image[y, x] == 255:
-            image[y, x] = id
+        if stringMatrix[y, x] == tileTypeString:
+            stringMatrix[y, x] = id
 
-            if x + 1 < image.shape[1] and image[y, x + 1] == 255:
+            if x + 1 < stringMatrix.shape[1] and stringMatrix[y, x + 1] == tileTypeString:
                 burn_queue.append((y, x + 1))
-            if y + 1 < image.shape[0] and image[y + 1, x] == 255:
+            if y + 1 < stringMatrix.shape[0] and stringMatrix[y + 1, x] == tileTypeString:
                 burn_queue.append((y + 1, x))
-            if x - 1 >= 0 and image[y, x - 1] == 255:
+            if x - 1 >= 0 and stringMatrix[y, x - 1] == tileTypeString:
                 burn_queue.append((y, x - 1))
-            if y - 1 >= 0 and image[y - 1, x] == 255:
+            if y - 1 >= 0 and stringMatrix[y - 1, x] == tileTypeString:
                 burn_queue.append((y - 1, x))
 
-        #print(image)
+        #print(stringMatrix)
         #print(burn_queue)
         #input()
 
@@ -50,6 +50,9 @@ def grassfire(image):
     for y, row in enumerate(image):
         for x, pixel in enumerate(row):
             next_id = ignite_pixel(image, (y, x), next_id)
+
+#
+
 
 grassfire(img)
 
