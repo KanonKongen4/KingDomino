88effadd9d01f72fb30fbc3758ef3a933d5a72aa
KanonKongen4 <91434226+KanonKongen4@users.noreply.github.com>
2022-11-22 15:19:38 +0100

    No nonMaximaSupression:-C

--- NonMaximaSuppression.py
+++ NonMaximaSuppression.py
@@ -1,17 +1,36 @@
-def NMS(boxes, overlapThresh):
 
-    areas = []
-    for box in boxes:
+# def isWithinRange(area1, area2, range):
+#     if area1>area2-range and area1< area2 + range:
+#         return True
+#     else:
+#         return False
 
-        x1 = boxes[0]
-        y1 = boxes[1]
-        x2 = boxes[3]
-        y2 = boxes[4]
-
-        width = x2-x1
-        height = y2-y1
-        print(box)
-
-        areas.append()
+def NMS(boxes, overlapThresh): # TODO: started writing own NMS but its kinda hard to do:-O!!!
+    #
+    # areas = []
+    # topLeftPoints = []
+    #
+    # for box in boxes:
+    #
+    #     x1 = box[0]
+    #     y1 = box[1]
+    #     x2 = box[2]
+    #     y2 = box[3]
+    #
+    #     width = x2-x1
+    #     height = y2-y1
+    #
+    #     area = width*height
+    #     # print("x1/y1: ", x1,"/",y1,  "    area: ", area)
+    #     areas.append(area)
+    #
+    #     listPoint1 =[x1,y1]
+    #     # tup = tuple(x1,y1)
+    #     topLeftPoints.append(listPoint1)
+    #
+    # # for i, area in enumerate(areas):
+    # #     if isWithinRange(area)
+    # print(areas)
+    # print(topLeftPoints)
 
     return boxes
