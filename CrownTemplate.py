d3aefd5f0112e8e9c5bd3c07efb80da8f3ca3d98
KanonKongen4 <91434226+KanonKongen4@users.noreply.github.com>
2022-11-22 11:33:04 +0100

    Working template matching!

--- CrownTemplate.py
+++ CrownTemplate.py
@@ -13,11 +13,9 @@ input_dir_left = Path.cwd() / "crownImages/left"
 #Add the images to lists
 ex_images_up = list(input_dir_up.glob('*.jpg'))  # Finds all images in folder  .png extension and adds them to an array
 ex_images_right = list(input_dir_right.glob('*.jpg'))
-ex_images_down = list(input_dir_up.glob('*.jpg'))
+ex_images_down = list(input_dir_down.glob('*.jpg'))
 ex_images_left = list(input_dir_left.glob('*.jpg'))
 
-for crown_image in ex_images_left:
-    print(crown_image)
 
 #Image To Search for crowns in
 imgsearch = cv.imread('20.jpg')
@@ -25,19 +23,25 @@ imgsearch = cv.imread('20.jpg')
 img_gray = cv.cvtColor(imgsearch, cv.COLOR_BGRA2GRAY)
 #width and height of image
 
-DEFAULT_THRESHOLD = 0.5
+DEFAULT_THRESHOLD = 0.8
 detections = []
 def Find_Crowns_in_Image(gray_image, listOfExampleCrowns):
     for crown_image in listOfExampleCrowns: # Loop over every image in the list
-        w, h = crown_image.shape[::-1] # finding the width and height of the image ??
-        res_left = cv.matchTemplate(gray_image, crown_image, cv.TM_CCOEFF_NORMED)
-        loc = numpy.where(res_left >= DEFAULT_THRESHOLD)
-        for pt in zip(*loc[::-1]):
-            cv.rectangle(gray_image, pt, (pt[0] + w, pt[1] + h), (0, 0, 225), 2)
-            detections.append(pt)
-    return  gray_image
+        img = cv.imread(str(crown_image))[:, :, :] # Read the image
+        gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY) # convert crown example image to grayscale
+        w, h  = gray.shape # get width and height of example image
+        result = cv.matchTemplate(gray_image, gray, cv.TM_CCOEFF_NORMED) #??
+        loc = numpy.where(result >= DEFAULT_THRESHOLD) # numpy.where loops over the image and checks if result > threshold
+        for point in zip(*loc[::-1]): #??
+            cv.rectangle(imgsearch, point, (point[0] + w, point[1] + h), (0, 0, 225), 2) # Draw rectangle at top-left point of match
+            detections.append(point) # add point to list
+    return  imgsearch
 
 image = Find_Crowns_in_Image(img_gray,ex_images_up)
+image = Find_Crowns_in_Image(img_gray,ex_images_right)
+image = Find_Crowns_in_Image(img_gray,ex_images_down)
+image = Find_Crowns_in_Image(img_gray,ex_images_left)
+
 cv.imshow("image", image)
 print(detections)
 
